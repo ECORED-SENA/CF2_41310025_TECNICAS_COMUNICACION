29b29a5858d58d86ed95ed92301a4ba8a1847af8
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-09-03 22:23:00 -0500

    avances

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -2,7 +2,7 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 1
       h1 Documentos escritos
@@ -10,13 +10,13 @@
     .fondo-personalizado-1.top-1.f1
       .fondo-personalizado-1__contenido.pb-5
         .row.align-items-center.mb-4
-          .col-lg-5
+          .col-lg-5(data-aos="fade-right")
             figure.mb-4.mb-lg-0
               img.mx-auto(src="@/assets/curso/temas/4.png",style="max-width: 505px;")
-          .col-lg-7
+          .col-lg-7(data-aos="fade-left")
             p.mb-0 Los documentos escritos son el resultado de la comunicación realizada en papel o mediante una computadora. Es uno de los métodos más empleados para entablar una comunicación, en el cual #[span.texto-p-1 el emisor elabora diversos tipos de textos para transmitir su mensaje de la mejor manera posible], con el objetivo de que lleguen al mayor número de receptores de manera óptima y oportuna.
     
-        .fondo-color-sistema-k.p-4.p-lg-0.ps-lg-5.borde-p-1.sombra-p-1.mb-4
+        .fondo-color-sistema-k.p-4.p-lg-0.ps-lg-5.borde-p-1.sombra-p-1.mb-4(data-aos="fade-up")
           .row 
             .col-lg-7.align-content-center
               p.mb-0.py-lg-4 Esta comunicación, establecida entre #[span.fw-bold un emisor y un receptor,] no ocurre de manera inmediata o, en ocasiones, puede que nunca se concrete, lo que permite que el escrito #[span.fw-bold perdure en el tiempo]. Esta es una de las ventajas de este tipo de comunicación, que puede considerarse permanente, ya que no se desvanece ni se olvida como las palabras.
@@ -25,27 +25,27 @@
                 img.mx-auto.d-none.d-lg-block(src="@/assets/curso/temas/5.svg")
                 img.mx-auto.d-lg-none(src="@/assets/curso/temas/6.svg")
 
-        .row.align-items-center.pb-4
+        .row.align-items-center.pb-4(data-aos="fade-down")
           .col-md-auto
             figure.mb-4.mb-md-0
               img.mx-auto(src="@/assets/curso/temas/7.svg",style="max-width: 82px;")
           .col 
             p.mb-0 En el ámbito empresarial, es importante reconocer la diversidad de formatos que permiten comunicar distintos mensajes según los intereses y condiciones del contexto. Por ello, el conocimiento y la implementación de diversos formatos y estructuras de documentos escritos son esenciales para desarrollar un buen plan de comunicación empresarial.
 
-    #t_1_1.titulo-segundo.color-acento-contenido
+    #t_1_1.titulo-segundo.color-acento-contenido(data-aos="fade")
       h2 1.1 Carta
     
     .row.mb-4
       .col-lg-6
-        p.mb-4 Un documento con estructura definida se convierte en un medio de comunicación que busca cumplir diversos objetivos comunicativos, como un saludo, una petición, un mensaje de reconocimiento o de afecto, entre otros. Debido a esto, las cartas tienen una estructura que varía según el propósito para el cual se redactan y a quiénes van dirigidas.
+        p.mb-4(data-aos="fade-right") Un documento con estructura definida se convierte en un medio de comunicación que busca cumplir diversos objetivos comunicativos, como un saludo, una petición, un mensaje de reconocimiento o de afecto, entre otros. Debido a esto, las cartas tienen una estructura que varía según el propósito para el cual se redactan y a quiénes van dirigidas.
 
-        p.mb-0 Antes de escribir una carta, es fundamental considerar el contexto de comunicación y responder las siguientes preguntas:
+        p.mb-0(data-aos="fade-right") Antes de escribir una carta, es fundamental considerar el contexto de comunicación y responder las siguientes preguntas:
       .col-lg-6.order-first.order-lg-last
-        figure.mb-4.mb-lg-0
+        figure.mb-4.mb-lg-0(data-aos="fade-left")
           img.mx-auto(src="@/assets/curso/temas/8.png",style="max-width: 610px;")
 
     .row.justify-content-center 
-      .col-lg-4.col-md-6.mb-4
+      .col-lg-4.col-md-6.mb-4(data-aos="fade-up")
         .fondo-color-sistema-m.borde-23.p-4.h-100
           .row.align-items-center.h-100
             .col-lg-5
@@ -53,7 +53,7 @@
                 img.mx-auto(src="@/assets/curso/temas/9.svg")
             .col-lg-7
               p.mb-0.fw-bold ¿A quién va dirigida?
-      .col-lg-4.col-md-6.mb-4
+      .col-lg-4.col-md-6.mb-4(data-aos="fade-down")
         .fondo-color-sistema-m.borde-23.p-4.h-100
           .row.align-items-center.h-100
             .col-lg-5
@@ -61,7 +61,7 @@
                 img.mx-auto(src="@/assets/curso/temas/10.svg")
             .col-lg-7
               p.mb-0.fw-bold ¿Debe el discurso ser formal o se puede emplear un tono más informal?
-      .col-lg-4.col-md-6.mb-4
+      .col-lg-4.col-md-6.mb-4(data-aos="fade-up")
         .fondo-color-sistema-m.borde-23.p-4.h-100
           .row.align-items-center.h-100
             .col-lg-5
@@ -70,7 +70,7 @@
             .col-lg-7
               p.mb-0.fw-bold ¿Cuál es el objetivo de la carta? (informar, saludar, expresar sentimientos, persuadir, pedir, protestar).
     
-    .row.justify-content-center
+    .row.justify-content-center(data-aos="flip-up")
       .col-lg-5.pe-lg-0
         .fondo-color-secundario.h-100.p-4.px-md-5.align-content-center
           p.mb-0.text-white.fw-bold Siempre será necesario revisar la claridad del texto, la ortografía y la presentación.
@@ -79,18 +79,18 @@
           figure
             img.mx-auto(src="@/assets/curso/temas/12.svg",style="max-width: 105px;")
 
-    .titulo-decorativo.my-5
+    .titulo-decorativo.my-5(data-aos="fade-right")
       .cont-figura
         img(src="@/assets/curso/temas/img-tit-dec.svg")
       .cont-text 
         h3 Elementos indispensables en una carta
         .cont-text__linea
     
-    p.mb-4 Para redactar una carta, se deben incluir los siguientes elementos:
+    p.mb-4(data-aos="fade") Para redactar una carta, se deben incluir los siguientes elementos:
 
     .fondo-personalizado-1.size-full.pos-center.f2
       .fondo-personalizado-1__contenido
-        .row.justify-content-center.mb-4.align-items-center
+        .row.justify-content-center.mb-4.align-items-center(data-aos="flip-up")
           .col-lg-7
             .fondo-color-primario.p-4
               .row.align-items-center 
@@ -113,7 +113,7 @@
             figure.mb-4.mb-lg-0
               img.mx-auto(src="@/assets/curso/temas/13.png",style="max-width: 296px;")
     
-        .fondo-color-sistema-k.p-4.p-lg-0.pe-lg-5.sombra-p-1.mb-4.borde-18
+        .fondo-color-sistema-k.p-4.p-lg-0.pe-lg-5.sombra-p-1.mb-4.borde-18(data-aos="flip-up")
           .row 
             .col-lg-7.align-content-center
               p.mb-4.fw-bold.pt-lg-4 ¿Cómo escribir una carta formal?
@@ -125,19 +125,19 @@
 
     Separador.mt-5
 
-    #t_1_2.titulo-segundo.color-acento-contenido
+    #t_1_2.titulo-segundo.color-acento-contenido(data-aos="fade-right")
       h2 1.2 El informe
     
     .row.align-items-center.mb-4
-      .col-md-auto 
+      .col-md-auto(data-aos="fade-right")
         figure.mb-4.mb-md-0
           img.mx-auto(src="@/assets/curso/temas/17.svg",style="max-width: 85px;")
-      .col 
+      .col(data-aos="fade-left")
         p.mb-0 Este informe tiene como objetivo analizar y documentar los aspectos clave necesarios para el desarrollo exitoso de un proyecto. A través de un enfoque metódico de comunicación, se pretende garantizar que todos los elementos importantes sean cubiertos y comprendidos por los involucrados. Para asegurar la efectividad del informe, es fundamental seguir una guía estructurada que contemple los siguientes aspectos:
 
     .fondo-personalizado-1.no-tam-full.f3.borde-20.overflow-hidden.mb-4
       .fondo-personalizado-1__contenido.p-4
-        SlyderA.slider-personalizado-1.pt-lg-3(tipo="b")
+        SlyderA.slider-personalizado-1.pt-lg-3(tipo="b" data-aos="zoom-in-out")
           .row.justify-content-center.align-items-center
             .col-lg-6.mb-4.mb-lg-0
               h4.mb-4.pt-lg-4 Ubicación de elementos visuales
@@ -160,14 +160,14 @@
               figure
                 img.mx-auto(src="@/assets/curso/temas/21.png",style="max-width: 460px;")
 
-    p.mb-0 Este enfoque garantizará que el informe no solo documente los hallazgos, sino que también sirva como una herramienta útil para guiar futuras acciones y mejorar la calidad del proyecto en desarrollo.
+    p.mb-0(data-aos="fade") Este enfoque garantizará que el informe no solo documente los hallazgos, sino que también sirva como una herramienta útil para guiar futuras acciones y mejorar la calidad del proyecto en desarrollo.
 
     Separador
 
-    #t_1_3.titulo-segundo.color-acento-contenido
+    #t_1_3.titulo-segundo.color-acento-contenido(data-aos="fade-right")
       h2 1.3 El memorando
 
-    .fondo-color-sistema-o.p-4.ps-lg-0.mb-4
+    .fondo-color-sistema-o.p-4.ps-lg-0.mb-4(data-aos="fade-up")
       .row 
         .col-lg-5
           figure.mb-4.mb-lg-0
@@ -175,41 +175,55 @@
         .col-lg-7
           p.mb-0 El memorando, derivado del latín #[em memorándum] (que significa "algo que debe recordarse"), es un documento fundamental en el ámbito de la comunicación interna dentro de las organizaciones. Este formato de comunicación escrita se caracteriza por su precisión y claridad, siendo utilizado principalmente para informar, solicitar o recomendar acciones en asuntos que requieren especial atención.
     
-    p.mb-4 La estructura del memorando es una de sus características más importantes, ya que debe seguir un formato preestablecido que permita transmitir información de manera eficaz y comprensible. Este formato generalmente incluye los siguientes componentes:
+    p.mb-4(data-aos="fade-up") La estructura del memorando es una de sus características más importantes, ya que debe seguir un formato preestablecido que permita transmitir información de manera eficaz y comprensible. Este formato generalmente incluye los siguientes componentes:
 
-    ImagenInfografica.color-primario
-      template(v-slot:imagen)
-        figure.mb-4.mb-lg-0
-          img.mx-auto(src="@/assets/curso/temas/23.svg",style="max-width: 839px;")
+    .fondo-personalizado-1.no-tam-full.f4.mb-5
+      .fondo-personalizado-1__contenido.p-4.p-lg-5
+        .row.justify-content-center(data-aos="zoom-in-out")
+          .col-lg-9
+            ImagenInfografica.imagen-infografica-personalizada-1.color-acento-botones
+              template(v-slot:imagen)
+                figure.mb-4.mb-lg-0
+                  img.mx-auto(src="@/assets/curso/temas/23.svg")
 
-      .tarjeta.color-acento-botones.p-3(x="20%" y="20%" numero="1")
-        .h5.mb-2 titulo
-        p Lorem ipsum dolor Curabitur vestibulum tortor at blandit congue. Pellentesque vel felis posuere, molestie metus
-
-      .tarjeta.color-acento-botones.p-3(x="50%" y="50%" numero="A")
-        .h5.mb-2 titulo
-        p Lorem ipsum dolor ddasdasd asdasd asdasd a asd sd asdasd sit amet, consectetur adipiscing elit. Curabitur vestibulum tortor at blandit congue. Pellentesque vel felis posuere, molestie metus
-
-      .tarjeta.color-acento-botones.p-3(x="70%" y="70%" numero="B")
-        .h5.mb-2 titulo
-        p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum tortor at blandit congue. Pellentesque vel felis posuere, molestie metus
+              .tarjeta.fondo-color-sistema-p.borde-9.p-3.punto-i-1(x="75%" y="7.7%" numero="+")
+                .h5.mb-4 Encabezado
+                p.mb-0 Detalla los elementos básicos del memorando, como el destinatario, el remitente, la fecha y el asunto. Es crucial para situar rápidamente el contexto del documento y facilitar su identificación.
+              .tarjeta.fondo-color-sistema-p.borde-9.p-3.punto-i-1(x="95%" y="16%" numero="+")
+                .h5.mb-4 Cuerpo del memorando
+                p.mb-0 Es la sección donde se desarrolla el contenido principal. Debe estar organizado de manera lógica y coherente, dividido en párrafos o secciones claras que expongan los hechos, razones y circunstancias relevantes. El contenido debe ser directo y conciso.
+              .tarjeta.fondo-color-sistema-p.borde-9.p-3.punto-i-1(x="5%" y="28.6%" numero="+")
+                .h5.mb-4 Hechos
+                p.mb-0 Exposición objetiva y detallada de los hechos relevantes, proporcionando la información necesaria para que el lector comprenda el contexto y la situación abordada.
+              .tarjeta.fondo-color-sistema-p.borde-9.p-3.punto-i-1(x="92%" y="44.5%" numero="+")
+                .h5.mb-4 Razones
+                p.mb-0 Presentación de las razones que sustentan la posición o la acción recomendada, argumentando de forma lógica por qué se considera necesario actuar de cierta manera.
+              .tarjeta.fondo-color-sistema-p.borde-9.p-3.punto-i-1(x="0%" y="59%" numero="+")
+                .h5.mb-4 Circunstancias
+                p.mb-0 Consideración de las circunstancias que rodean el tema tratado, incluyendo limitaciones, riesgos o factores externos que puedan influir en la toma de decisiones.
+              .tarjeta.fondo-color-sistema-p.borde-9.p-3.punto-i-1(x="100%" y="72.5%" numero="+")
+                .h5.mb-4 Conclusión y recomendaciones
+                p.mb-0 Síntesis de lo discutido con recomendaciones específicas para la acción, orientando al lector en los próximos pasos a seguir.
+              .tarjeta.fondo-color-sistema-p.borde-9.p-3.punto-i-1(x="0%" y="87%" numero="+")
+                .h5.mb-4 Firma o iniciales del remitente
+                p.mb-0 Puede incluirse para formalizar el documento y confirmar su autenticidad.
 
     
     .row.align-items-center
-      .col-lg-5
+      .col-lg-5(data-aos="fade-right")
         figure.mb-4.mb-lg-0
           img.mx-auto(src="@/assets/curso/temas/24.png",style="max-width: 505px;")
-      .col-lg-7
+      .col-lg-7(data-aos="fade-left")
         figure.mb-4
           img.me-auto(src="@/assets/curso/temas/25.svg",style="max-width: 85px;")
         p.mb-0 El memorando es una herramienta clave en la gestión organizacional, ya que permite documentar y comunicar de manera formal y estructurada los aspectos que deben ser considerados en decisiones importantes o en situaciones que revisten cierta gravedad. Su claridad, precisión y capacidad para registrar información crítica hacen del memorando un recurso indispensable en la administración de cualquier entidad.
     
     Separador
 
-    #t_1_4.titulo-segundo.color-acento-contenido
+    #t_1_4.titulo-segundo.color-acento-contenido(data-aos="fade-right")
       h2 1.4 El acta
 
-    .bloque-texto-g.bloque-texto-g--inverso.color-primario.p-3.p-sm-4.p-md-5
+    .bloque-texto-g.bloque-texto-g--inverso.color-primario.p-3.p-sm-4.p-md-5(data-aos="fade-left")
       .bloque-texto-g__img(
         :style="{'background-image': `url(${require('@/assets/curso/temas/26.png')})`}"
       )
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -2,10 +2,134 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 La argumentación
+    
+    .row.align-items-center 
+      .col-lg-5(data-aos="fade-right")
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/28.png",style="max-width: 505px;")
+      .col-lg-7(data-aos="fade-left")
+        p.mb-4 Argumentar es una técnica de comunicación fundamental que tiene como objetivo explicar o expandir diferentes contenidos relacionados, fortaleciendo así la coherencia de las ideas presentadas por un orador o escritor. La argumentación facilita la correspondencia entre diversas ideas, utilizando las reglas del pensamiento crítico para obtener información nueva.
+
+        .fondo-color-sistema-q.p-4.pb-3.pe-3
+          figure.mb-4
+            img.me-auto(src="@/assets/curso/temas/29.svg",style="max-width: 45px;")
+          p.fw-bold.text-center.mb-3 Según Álvarez (2005, p. 23), el propósito principal de los textos argumentativos es legitimar explícitamente la nueva información proporcionada, ya sea a través de datos empíricos, razonamientos, pruebas o datos concretos y verificables.
+          figure
+            img.ms-auto(src="@/assets/curso/temas/30.svg",style="max-width: 38px;")
+    
+    .titulo-decorativo.my-5(data-aos="fade-right")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Organización de la argumentación
+        .cont-text__linea
+    
+    p.mb-4(data-aos="fade-up") Existen diversas formas de organizar una argumentación, aunque el orden puede variar según la intención del expositor. Sin embargo, hay elementos clave que deben estar presentes en cualquier argumentación efectiva.
+
+    TabsB.tabs-p-1.color-acento-contenido(data-aos="flip-up")
+      .row.justify-content-center.align-items-center.py-4.py-lg-5(titulo="Presentación de la tesis" :icono="require('@/assets/curso/temas/31.svg')")
+        .col-lg-6.mb-4.mb-lg-0
+          h4.mb-4 Presentación de la tesis
+          p.mb-0 Es esencial comenzar con la tesis de manera precisa, siguiendo los principios de brevedad y claridad. Esta es la afirmación central que guiará todo el discurso.
+        .col-lg-5
+          figure
+            img.mx-auto(src="@/assets/curso/temas/32.png",style="max-width: 460px;")
+      .row.justify-content-center.align-items-center.py-4.py-lg-5(titulo="Desarrollo del cuerpo argumentativo" :icono="require('@/assets/curso/temas/35.svg')")
+        .col-lg-6.mb-4.mb-lg-0
+          h4.mb-4 Desarrollo del cuerpo argumentativo
+          p.mb-0 El cuerpo de la argumentación debe ser un discurso más extenso que exponga las razones y argumentos que apoyan la tesis. Aquí se presentan las evidencias, ejemplos y razonamientos que fortalecen la afirmación principal.
+        .col-lg-5
+          figure
+            img.mx-auto(src="@/assets/curso/temas/33.png",style="max-width: 460px;")
+      .row.justify-content-center.align-items-center.py-4.py-lg-5(titulo="Conclusión" :icono="require('@/assets/curso/temas/36.svg')")
+        .col-lg-6.mb-4.mb-lg-0
+          h4.mb-4 Conclusión
+          p.mb-0 La argumentación debe culminar con una conclusión que reafirme la tesis, cerrando el discurso de manera convincente.
+        .col-lg-5
+          figure
+            img.mx-auto(src="@/assets/curso/temas/34.png",style="max-width: 460px;")
+    
+    .titulo-decorativo.my-5(data-aos="fade")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Normas para una buena argumentación
+        .cont-text__linea
+    
+    p.mb-4(data-aos="fade-down") Descubran cómo construir argumentos sólidos y persuasivos siguiendo las claves del pensamiento crítico:
+
+    figure(data-aos="zoom-in-up")
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/SqlRrZ6Aj18?si=v8jmfLACguVHGzeZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
+      figcaption Video. Normas para una buena argumentación
+    
+    .titulo-decorativo.my-5(data-aos="fade-left")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Tipos de premisas
+        .cont-text__linea
+    
+    p.mb-5(data-aos="fade-up") Existen dos tipos de premisas que sustentan cualquier argumentación: los fundamentos o conclusiones concluyentes y los fundamentos o conclusiones parciales.
+
+    .fondo-personalizado-1.size-full.med.pos-center.f5
+      .fondo-personalizado-1__contenido
+
+        h4.titulo-dec-2.py-1.px-3.mb-4(data-aos="fade-right") Fundamentos o conclusiones concluyentes
+
+        .row.align-items-center.mb-2
+          .col-lg-8
+            p.mb-4(data-aos="fade-up") Estos fundamentos determinan que la verdad de todas las premisas garantiza la verdad de la conclusión.
+            .row
+              .col-lg-6.mb-4(data-aos="fade-right")
+                .fondo-color-primario.borde-23.p-4.h-100
+                  .row.align-items-center.h-100
+                    .col-lg-4
+                      figure.mb-4.mb-lg-0
+                        img.mx-auto(src="@/assets/curso/temas/38.svg",style="max-width: 104px;")
+                    .col-lg-8
+                      h5.mb-4.text-white Tipo de argumento:
+                      p.mb-0.text-white Corresponden a los argumentos deductivos.
+              .col-lg-6.mb-4(data-aos="fade-left")
+                .fondo-color-primario.borde-23.p-4.h-100
+                  .row.align-items-center.h-100
+                    .col-lg-4
+                      figure.mb-4.mb-lg-0
+                        img.mx-auto(src="@/assets/curso/temas/39.svg",style="max-width: 104px;")
+                    .col-lg-8
+                      h5.mb-4.text-white Validez:
+                      p.mb-0.text-white Aquellos que cumplen con esta propiedad se consideran válidos; los que no, inválidos.
+          .col-lg-4.order-first.order-lg-last
+            figure.mb-4.mb-lg-0(data-aos="fade-down")
+              img.mx-auto(src="@/assets/curso/temas/37.png",style="max-width: 400px;")
+        
+        h4.titulo-dec-2.py-1.px-3.mb-4(data-aos="fade") Fundamentos o conclusiones parciales
+
+        p.mb-4(data-aos="fade") Se construyen a partir de premisas que proporcionan solo un apoyo parcial a la conclusión.
+
+        .row.justify-content-center
+          .col-lg-4.col-md-6.mb-4(data-aos="flip-up")
+            figure.mb-4.mb-md-0
+              img.mx-auto(src="@/assets/curso/temas/40.png",style="max-width: 400px;")
+          .col-lg-4.col-md-6.mb-4(data-aos="flip-up")
+            .borde-23.fondo-color-sistema-q.hover-1.p-4.pt-lg-5.h-100
+              figure.mb-4
+                img.mx-auto(src="@/assets/curso/temas/41.svg",style="max-width: 98px;")
+              h5.text-center.mb-4 Tipo de argumento
+              p.mb-0.text-center Estos son los argumentos inductivos, que buscan demostrar una conclusión mediante un apoyo parcial entre una idea (premisa) y otra.
+          .col-lg-4.col-md-6.mb-4(data-aos="flip-up")
+            .borde-23.fondo-color-sistema-q.hover-1.p-4.pt-lg-5.h-100
+              figure.mb-4
+                img.mx-auto(src="@/assets/curso/temas/42.svg",style="max-width: 98px;")
+              h5.text-center.mb-4 Corrección
+              p.mb-0.text-center Cuando se logra la conexión entre premisas y conclusiones, los argumentos se consideran correctos; de lo contrario, si la relación es incorrecta, los argumentos se consideran incorrectos.
+    
+    p.mb-0(data-aos="fade") La práctica constante y el respeto por las normas de inferencia y las reglas de interacción son esenciales para desarrollar una argumentación efectiva. Con el tiempo, estas habilidades pueden mejorar significativamente, permitiendo una comunicación más clara, coherente y persuasiva.
+        
 
 </template>
 
